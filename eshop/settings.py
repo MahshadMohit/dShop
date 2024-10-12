65717529d63c507064acfd4d07457006e8da88c0
Asoo <126341039+Maashooly@users.noreply.github.com>
2024-10-12 19:16:21 +0330

    enable sqlite3

--- eshop/settings.py
+++ eshop/settings.py
@@ -78,7 +78,9 @@ WSGI_APPLICATION = 'eshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+
         'NAME': BASE_DIR / 'db.sqlite3',
+
     }
 }
 
--- product/models.py
+++ product/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 # Create your models here.
 
-class Product(models.Model):
+class Product(models.Model):  # name of table
     title = models.CharField(max_length=300)
-    price = models.IntegerField
+    price = models.IntegerField()
